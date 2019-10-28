b7e72819f145662e28f304895c3a37a22ea1cdcc
Nyorok <vi.maschio@outlook.com>
2019-10-28 20:26:00 -0300

    Initial FeixeLocal commit

--- controller1/controller.py
+++ controller1/controller.py
@@ -1,5 +1,6 @@
 import controller_template as controller_template
 import numpy as np
+import random
 
 
 class Controller(controller_template.Controller):
@@ -30,7 +31,7 @@ class Controller(controller_template.Controller):
         computed_values = features * parameters
         summed_values = np.sum(computed_values, axis=1)
 
-        return np.argmax(summed_values)
+        return np.argmax(summed_values) + 1
 
     def compute_features(self, sensors):
         """
@@ -61,6 +62,7 @@ class Controller(controller_template.Controller):
             enemy_detected = sensors
 
         # Compute features
+        constant_f = 1
         approx_f = self.normalize(checkpoint_distance -
                                   self.old_next_checkpoint, -200, 200)
         left_f = self.normalize(track_distance_left, 0, 100)
@@ -75,8 +77,8 @@ class Controller(controller_template.Controller):
         # Time without choosing to turn
         # Faster than threshold
 
-        features = np.array([approx_f, left_f, center_f,
-                             right_f, ontrack_f, velocity_f, 0, 0])
+        features = np.array([constant_f, approx_f, left_f, center_f,
+                             right_f, ontrack_f, velocity_f])
 
         # Update values
         self.old_next_checkpoint = checkpoint_distance
@@ -91,17 +93,101 @@ class Controller(controller_template.Controller):
         :param weights: initial weights of the controller (either loaded from a file or generated randomly)
         :return: the best weights found by your learning algorithm, after the learning process is over
         """
+        def generate_neighbours(best_weights, epsilon):
+            neighbours = []
+            for j in range(len(best_weights)):
+                for i in range(len(best_weights[j])):
+                    new_neighbour = best_weights[j].copy()
+                    new_neighbour[i] += random.random()*epsilon
+                    neighbours.append(new_neighbour.copy())
+                for i in range(len(best_weights[j])):
+                    new_neighbour = best_weights[j].copy()
+                    new_neighbour[i] -= random.random() * epsilon
+                    neighbours.append(new_neighbour.copy())
+            print(len(neighbours), "novos vizinhos gerados.")
+            return neighbours
 
-        best_value = self.run_episode(weights)
-        best_weights = np.array(weights).reshape(5, -1)
+        def compute_best_neighbours(neighbours, highest_values, highest_weights, k_inst):
+            print()
+            print("Computing ", len(neighbours), " neighbours.")
+            for i in range(1,len(neighbours)):
+                new_value = self.run_episode(neighbours[i])
+                for j in range(k_inst):
+                    if new_value > highest_values[j]:
+                        print()
+                        print("New best value", j, "found:", new_value, ">", highest_values[j])
+                        new_weights = []
+                        new_values = []
+                        if j == 0:
+                            new_weights.append(neighbours[i].copy())
+                            new_weights.extend(highest_weights[1:k_inst].copy())
 
+                            new_values.append(new_value)
+                            new_values.extend(highest_values[1:k_inst])
+                        else:
+                            new_weights.extend(highest_weights[0:j])
+                            new_weights.append(neighbours[i].copy())
+
+                            new_values.extend(highest_values[0:j])
+                            new_values.append(new_value)
+
+                            if j+1 != k_inst:
+                                new_weights.extend(highest_weights[j + 1:k_inst])
+                                new_values.extend(highest_values[j + 1:k_inst])
+
+                        highest_weights = new_weights.copy()
+                        highest_values = new_values.copy()
+                        print("Highest Values:", highest_values)
+                        break
+                print("Vizinho", i, "calculado como", new_value)
+            return highest_values.copy(), highest_weights.copy()
+
+
+        #best_value = self.run_episode(weights)
+        #best_weights = np.array(weights).reshape(5, -1)
         # Learning process
+        k_inst = 10
+        iter = 0
+        iter_unchanged = 0
+        epsilon = 20
+
+        highest_values = []
+        highest_weights = []
+        highest_weights.append(weights.copy())
+        highest_values.append(self.run_episode(weights))
+        for j in range(1, k_inst):
+            highest_weights.append([random.uniform(-1, 1) for i in range(0, len(weights))])
+            highest_values.append(self.run_episode(highest_weights[j]))
+
+
+
         try:
             while True:
-                pass
+                print()
+                print("Iteration", iter, "after", iter_unchanged, "unchanged iterations. Epsilon actual value is", epsilon)
+                print()
+
+                old_weights = highest_weights.copy
+                highest_values, highest_weights = compute_best_neighbours( generate_neighbours(highest_weights, epsilon),
+                                                                              highest_values, highest_weights, k_inst)
+                print()
+                print("Highest Score: ", highest_values[0], highest_values[1], highest_values[2])
+                print()
+                if np.sum(old_weights) == np.sum(highest_weights):
+                    iter_unchanged +=1
+                else:
+                    iter_unchanged = 0
+                iter += 1
+                if iter_unchanged > 3:
+                    epsilon *= 0.5
+                    if epsilon < 0.0001:
+                        epsilon = 100
+
+
         except KeyboardInterrupt:  # To be able to use CTRL+C to stop learning
             pass
 
+        best_weights = np.array(weights).reshape(5, -1)
         # raise NotImplementedError("This Method Must Be Implemented")
 
         # Return the weights learned at this point
