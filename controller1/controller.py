0dc67c3ee759e9f021d5ad52718d21464523107b
Nyorok <vi.maschio@outlook.com>
2019-10-28 20:13:25 -0300

    Inicial HillClimb commit

--- controller1/controller.py
+++ controller1/controller.py
@@ -1,5 +1,6 @@
 import controller_template as controller_template
 import numpy as np
+import random
 
 
 class Controller(controller_template.Controller):
@@ -30,7 +31,7 @@ class Controller(controller_template.Controller):
         computed_values = features * parameters
         summed_values = np.sum(computed_values, axis=1)
 
-        return np.argmax(summed_values)
+        return np.argmax(summed_values) + 1
 
     def compute_features(self, sensors):
         """
@@ -61,6 +62,7 @@ class Controller(controller_template.Controller):
             enemy_detected = sensors
 
         # Compute features
+        constant_f = 1
         approx_f = self.normalize(checkpoint_distance -
                                   self.old_next_checkpoint, -200, 200)
         left_f = self.normalize(track_distance_left, 0, 100)
@@ -75,8 +77,8 @@ class Controller(controller_template.Controller):
         # Time without choosing to turn
         # Faster than threshold
 
-        features = np.array([approx_f, left_f, center_f,
-                             right_f, ontrack_f, velocity_f, 0, 0])
+        features = np.array([constant_f, approx_f, left_f, center_f,
+                             right_f, ontrack_f, velocity_f])
 
         # Update values
         self.old_next_checkpoint = checkpoint_distance
@@ -91,16 +93,63 @@ class Controller(controller_template.Controller):
         :param weights: initial weights of the controller (either loaded from a file or generated randomly)
         :return: the best weights found by your learning algorithm, after the learning process is over
         """
+        def generate_neighbours(weights, epsilon):
+            neighbours = []
+            neighbours.append(weights.copy())
+            for i in range(len(weights)):
+                new_neighbour = weights.copy()
+                new_neighbour[i] += random.random() * epsilon
+                neighbours.append(new_neighbour.copy())
+            for i in range(len(weights)):
+                new_neighbour = weights.copy()
+                new_neighbour[i] -= random.random() * epsilon
+                neighbours.append(new_neighbour.copy())
+            return neighbours
 
-        best_value = self.run_episode(weights)
-        best_weights = np.array(weights).reshape(5, -1)
+        def compute_best_neighbour(neighbours):
+            print("Computing ", len(neighbours), " neighbours.")
+            best_value = self.run_episode(neighbours[0])
+            bestNeighbour = neighbours[0]
+            for i in range(1,len(neighbours)):
+                new_value = self.run_episode(neighbours[i])
+                if new_value > best_value:
+                    best_value = new_value
+                    bestNeighbour = neighbours[i].copy()
+            return best_value, bestNeighbour.copy()
 
+        #best_value = self.run_episode(weights)
+        #best_weights = np.array(weights).reshape(5, -1)
+        iter = 0
+        iter_unchanged = 0
+        epsilon = 20
+        best_weights = weights
+        best_value = self.run_episode(best_weights)
+        print("Best Score: ", best_value)
         # Learning process
         try:
             while True:
-                pass
+                print("Iteration", iter, "after", iter_unchanged, "unchanged iterations. Epsilon actual value is", epsilon)
+
+                old_weights = weights.copy()
+                best_value, weights = compute_best_neighbour(generate_neighbours(weights, epsilon))
+
+                print("Best Score: ", best_value)
+                print()
+
+                if np.sum(old_weights) == np.sum(weights):
+                    iter_unchanged +=1
+                else:
+                    iter_unchanged = 0
+                iter += 1
+                if iter_unchanged > 3:
+                    epsilon *= 0.5
+                    if epsilon < 0.0001:
+                        epsilon = 100
+
+
         except KeyboardInterrupt:  # To be able to use CTRL+C to stop learning
             pass
+
 
         # raise NotImplementedError("This Method Must Be Implemented")
 
