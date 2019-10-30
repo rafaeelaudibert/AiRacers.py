5f53dca371ae5b3cc79a417cf0ea8dc4507932a1
RafaAudibert - ML <rafaeelaudibert@gmail.com>
2019-10-30 10:31:18 -0300

    :recycle: Added new features

--- controller1/controller.py
+++ controller1/controller.py
@@ -5,6 +5,7 @@ MUTATION_RATIO = 0.5
 MUTATION_EPSILON = 1.0
 T = 250
 
+
 class Controller(controller_template.Controller):
     def __init__(self, track, evaluate=True, bot_type=None):
         super().__init__(track, evaluate=evaluate, bot_type=bot_type)
@@ -12,9 +13,13 @@ class Controller(controller_template.Controller):
         # This initialization value make it think we are moving forward in the 1st step
         self.old_next_checkpoint = float("-inf")
 
+        # Features hyperparamets
+        self.vel_threshold = 120
+        self.last_action = 5  # Nothing
+        self.no_turn_times = 0
+
         # Initialize population values
         self.elements = None
-        
 
     #######################################################################
     ##### METHODS YOU NEED TO IMPLEMENT ###################################
@@ -36,7 +41,11 @@ class Controller(controller_template.Controller):
 
         action_values = np.sum(features * parameters, axis=1)
 
-        return np.argmax(action_values) + 1
+        self.last_action = np.argmax(action_values) + 1
+        self.no_turn_times = self.no_turn_times + \
+            1 if self.last_action not in [1, 2] else 0
+
+        return self.last_action
 
     def compute_features(self, sensors):
         """
@@ -67,23 +76,26 @@ class Controller(controller_template.Controller):
             enemy_detected = sensors
 
         # Compute features
-        constant_f = 1
+        constant_f = 1.0
         approx_f = self.normalize(checkpoint_distance -
                                   self.old_next_checkpoint, -200, 200)
         left_f = self.normalize(track_distance_left, 0, 100)
         center_f = self.normalize(track_distance_center, 0, 100)
         right_f = self.normalize(track_distance_right, 0, 100)
+        central_f = self.normalize(
+            abs(track_distance_left - track_distance_right), 0, 100)
         ontrack_f = self.normalize(on_track, 0, 1)
         velocity_f = self.normalize(car_velocity, 0, 200)
+        slow_f = self.normalize(int(car_velocity < self.vel_threshold), 0, 1)
+        turn_f = self.normalize(max(self.no_turn_times, 10), 0, 10)
+        last_action_f = -1 if self.last_action == 1 else 1 if self.last_action == 2 else 0
 
         # TODO FEATURES
-        # Know how to leave grass
-        # Remember last action (left, center, right)
-        # Time without choosing to turn
-        # Faster than threshold
+        # Know how to leave grass <- Too hard, how?
 
         features = np.array([constant_f, approx_f, left_f, center_f,
-                             right_f, ontrack_f, velocity_f])
+                             right_f, central_f, ontrack_f, velocity_f,
+                             slow_f, turn_f, last_action_f])
 
         # Update values
         self.old_next_checkpoint = checkpoint_distance
@@ -99,15 +111,14 @@ class Controller(controller_template.Controller):
         :return: the best weights found by your learning algorithm, after the learning process is over
         """
 
-        
         # Compute feature_lens
-        
+
         ctrl_temp = Controller(self.track_name, bot_type=None, evaluate=False)
         fake_sensors = [53, 66, 100, 1, 172.1353274581511, 150, -1, 0, 0]
         features_len = len(ctrl_temp.compute_features(fake_sensors))
 
         elements = np.random.uniform(-1, 1, size=(PARENTS, features_len * 5))
-        
+
         # Generations
         gen = 0
 
@@ -115,9 +126,8 @@ class Controller(controller_template.Controller):
         fitness_exp = np.zeros((PARENTS))
 
         couples = np.zeros((PARENTS, 2))
-        
 
-        next_gen = np.zeros((PARENTS, features_len * 5)) # Auxiliar Array
+        next_gen = np.zeros((PARENTS, features_len * 5))  # Auxiliar Array
         best_weights = np.zeros((features_len * 5))
         best_fitness = max(fitness)
 
@@ -126,10 +136,10 @@ class Controller(controller_template.Controller):
             for i in range(PARENTS):
                 fitness[i] = self.run_episode(elements[i])
                 fitness_exp[i] = np.exp(fitness[i]/T)
-        
+
             exp_sum = sum(fitness_exp)
             while True:
-                
+
                 # Resets the offspring count
                 offspring = 0
 
@@ -139,31 +149,34 @@ class Controller(controller_template.Controller):
                 fitness = [val/exp_sum for val in fitness_exp]
 
                 # Couples from current generation
-                couples = np.random.choice(list(range(PARENTS)), p=fitness, size=(PARENTS, 2))
+                couples = np.random.choice(
+                    list(range(PARENTS)), p=fitness, size=(PARENTS, 2))
                 print("Coupĺes:")
                 print(*couples)
                 # Breeding
                 for offspring, (p1, p2) in enumerate(couples):
-                    for chromosome_it in range (features_len * 5): 
+                    for chromosome_it in range(features_len * 5):
                         next_gen[offspring, chromosome_it] = \
-                            np.random.choice([elements[p1, chromosome_it], elements[p2, chromosome_it]])
+                            np.random.choice(
+                                [elements[p1, chromosome_it], elements[p2, chromosome_it]])
 
                 elements = next_gen
 
                 # Mutation
                 for i in range(len(elements)):
                     for j in range(features_len * 5):
-                        elements[i, j] += np.random.choice([0, np.random.uniform(-1, 1) * MUTATION_EPSILON], p=[1 - MUTATION_RATIO, MUTATION_RATIO])
+                        elements[i, j] += np.random.choice([0, np.random.uniform(-1, 1) * MUTATION_EPSILON], p=[
+                                                           1 - MUTATION_RATIO, MUTATION_RATIO])
 
                 # Evaluate
                 for i in range(PARENTS):
                     fitness[i] = self.run_episode(elements[i])
                     fitness_exp[i] = np.exp(fitness[i]/T)
-                
+
                 exp_sum = sum(fitness_exp)
 
-                print ("Fitness Gen: ", gen)
-                print (*fitness)
+                print("Fitness Gen: ", gen)
+                print(*fitness)
 
                 max_fitness = max(fitness)
 
@@ -171,7 +184,7 @@ class Controller(controller_template.Controller):
                     best_fitness = max_fitness
                     best_weights = elements[fitness.index(max_fitness)]
                 print("Best Fitness:")
-                print (best_fitness)
+                print(best_fitness)
 
                 pass
 
